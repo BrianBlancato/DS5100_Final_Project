832b09b929c10c5aaf2ae498734b56beec6a98f8
Brian Blancato <brianblancato@gmail.com>
2023-04-30 17:20:02 -0400

    Code done

--- montecarlosimulator/montecarlo.py
+++ montecarlosimulator/montecarlo.py
@@ -2,70 +2,69 @@ import pandas as pd
 import numpy as np
 import random
 from typing import List
-from collections import Counter
-from itertools import combinations
+
 
 class Die:
     def __init__(self, faces):
-        self._df = pd.DataFrame({'face': faces, 'weight': [1.0] * len(faces)})
-        #self._df.set_index('face', inplace=True)
+        self.__die_df = pd.DataFrame({'face': faces, 'weight': [1.0] * len(faces)})
+        self.face_type = self.__die_df['face'].dtype
     
     def change_weight(self, face, new_weight):
-        if face not in self._df['face'].values:
+        if not (self.__die_df['face'] == face).any():
             raise ValueError('Invalid Face')
         try:
             new_weight = float(new_weight)
         except ValueError:
             raise ValueError('Invalid Weight')
-        self._df.loc[self._df['face'] == face, 'weight'] = new_weight
+        self.__die_df.loc[self.__die_df['face'] == face, 'weight'] = new_weight
 
     def roll(self, roll_count=1):
-        outcomes = []
+        roll_results = []
         for i in range(roll_count):
-            face = random.choices(self._df['face'], weights=self._df['weight'])[0]
-            outcomes.append(face)
-        return outcomes
+            face = random.choices(self.__die_df['face'], weights=self.__die_df['weight'])[0]
+            roll_results.append(face)
+        return roll_results
         
     def show(self):
-        return self._df
+        return self.__die_df
     
 class Game:
     def __init__(self, dice:List[Die]):
         self.dice = dice
-        self.results = None
+        self.__results = None
 
     def play(self, rolls):
-        self.results = pd.DataFrame(index=range(1, rolls+1), columns=range(1, len(self.dice)+1))
+        self.__results = pd.DataFrame(index=range(1, rolls+1), columns=range(1, len(self.dice)+1))
         for i, die in enumerate(self.dice):
-            self.results.iloc[:, i] = die.roll(rolls)
+            self.__results.iloc[:, i] = die.roll(rolls)
         
     def show(self, form: str = "wide"):
         if form =="wide":
-            return self.results
+            return self.__results
         elif form == "narrow":
-            return self.results.unstack().to_frame()
+            return self.__results.unstack().to_frame()
         else:
             raise ValueError("Invalid option for 'form'. Please choose either 'wide' or 'narrow'.")      
 
 class Analyzer:
     def __init__(self, game):
-        self.game = game
-        self.data = self.game.results
-        self.face_dtype = game.dice[0]._df.index.dtype
+        self.__data = game.show('wide')
+        self.face_dtype = game.dice[0].face_type
         self.jackpot_data = None
         self.face_counts = None
+        self.combos = None
     
     def jackpot(self):
-        self.jackpot_data = self.data[self.data.apply(lambda row: row.nunique() == 1, axis = 1)]
-        self.jackpot_data.index = self.data[self.data.apply(lambda row: row.nunique() == 1, axis = 1)].index
-        return sum(self.data.apply(lambda row: row.nunique() == 1, axis =1))
+        self.jackpot_data = self.__data[self.__data.apply(lambda row: row.nunique() == 1, axis = 1)]
+        self.jackpot_data.index = self.__data[self.__data.apply(lambda row: row.nunique() == 1, axis = 1)].index
+        return sum(self.__data.apply(lambda row: row.nunique() == 1, axis =1))
         
     def combo(self):
-        combos = self.data.groupby(list(self.data.columns)).size().rename('count')
-        combos = combos.reset_index().set_index(list(self.data.columns))
-        return combos
+        self.combos = self.__data.groupby(list(self.__data.columns)).size().rename('count')
+        self.combos = self.combos.reset_index().set_index(list(self.__data.columns))
+        return self.combos
     
     
     def face_counts_per_roll(self):
-        self.face_counts = self.data.apply(lambda row: row.value_counts(), axis=1, result_type='expand').fillna(0)
+        self.face_counts = self.__data.apply(lambda row: row.value_counts(), axis=1, result_type='expand').fillna(0)
         self.face_counts = self.face_counts.astype(int)
--- montecarlosimulator/montecarlo_tests.py
+++ montecarlosimulator/montecarlo_tests.py
@@ -10,50 +10,84 @@ class TestDie(unittest.TestCase):
         self.die = Die(faces)
 
     def test_change_weight(self):
-        self.die.change_weight(1, 6.0)
-        self.assertEqual(self.die.show().loc[1, 'weight'], 2.0)
+        self.die.change_weight(3, 3)
+        weight_for_face3 = self.die.show().loc[self.die.show()['face'] == 3, 'weight'].values[0]
+        self.assertEqual(weight_for_face3, 3.0)
+        with self.assertRaises(ValueError):
+            self.die.change_weight('A', 4)
+        with self.assertRaises(ValueError):
+            self.die.change_weight(2, 'elephant')
 
     def test_roll(self):
-        self.assertIn(self.die.roll()[0], self.die.show().index)
+        rolled_faces = self.die.roll(10)
+        valid_faces = self.die.show()['face'].values
+        for roll in rolled_faces:
+            self.assertIn(roll, valid_faces)
 
-def tesst_show(self):
-    self.assertIsInstance(self.die.sholw(), pd.DataFrame)
+    def test_show(self):
+        self.assertIsInstance(self.die.show(), pd.DataFrame)
+
 
 class TestGame(unittest.TestCase):
     def setUp(self):
         faces1 = [1, 2, 3, 4, 5, 6]
-        faces2 = ['A', 'B', 'C', 'D', 'E']
-        self.die1 = Die(faces1)
-        self.die2 = Die(faces2)
-        self.game = Game([self.die1, self.die2])
-        self.game.play(3)
+        self.test_die = Die(faces1)
+        self.game = Game([self.test_die, self.test_die, self.test_die])
     
-    def test_show_wide(self):
-        self.assertIsInstance(self.game.show("wide"), pd.DataFrame)
+    def test_play(self):
+        self.game.play(25)
+        results = self.game.show('wide')
+        self.assertIsInstance(results, pd.DataFrame)
+        self.assertEqual(results.shape, (25, 3))
+        self.assertEqual(list(results.index), list(range(1,26)))
+        self.assertEqual(list(results.columns), [1, 2, 3])
+        for i in range(1, 26):
+            for j in [1, 2, 3]:
+                self.assertIn(results.loc[i,j], [1, 2, 3, 4, 5, 6])
+
     
-    def test_show_narrow(self):
-        self.assertIsInstance(self.game.show("narrow"), pd.DataFrame)
+    def test_show(self):
+        self.game.play(10)
+        with self.assertRaises(ValueError):
+            self.game.show(form="invalid")
+        self.assertIsInstance(self.game.show(), pd.DataFrame)
+        wide_results = self.game.show('wide')
+        self.assertEqual(len(wide_results.columns), 3)
+        expected_wide_index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertListEqual(wide_results.index.tolist(), expected_wide_index)
+        narrow_results = self.game.show('narrow')
+        self.assertIsInstance(narrow_results.index, pd.MultiIndex)
+        self.assertEqual(len(narrow_results.columns), 1)
+
     
 class TestAnalyzer(unittest.TestCase):
     def setUp(self):
-        faces1 = [1, 2, 3, 4, 5, 6]
-        faces2 = ['A', 'B', 'C', 'D', 'E']
-        self.die1 = Die(faces1)
-        self.die2 = Die(faces2)
-        self.game = Game([self.die1, self.die2])
-        self.game.play(3)
+        faces = [1, 2, 3, 4, 5, 6]
+        self.die = Die(faces)
+        self.game = Game([self.die, self.die])
+        self.game.play(10)
         self.analyzer = Analyzer(self.game)
  
     def test_jackpot(self):
         self.assertIsInstance(self.analyzer.jackpot(), int)
+        if self.analyzer.jackpot() != 0:
+            self.assertIsInstance(self.analyzer.jackpot_data, pd.DataFrame)
+            self.assertTrue((self.analyzer.jackpot_data.nunique(axis=1) == 1).all())
+            for idx in self.analyzer.jackpot_data.index:
+                self.assertTrue(self.game.show().loc[idx].equals(self.analyzer.jackpot_data.loc[idx]))
 
     def test_combo(self):
-        self.assertIsInstance(self.analyzer.combo(), pd.DataFrame)
+        self.analyzer.combo()
+        self.assertIsInstance(self.analyzer.combos, pd.DataFrame)
+        self.assertIsInstance(self.analyzer.combos.index, pd.MultiIndex)
+        self.assertEqual(self.analyzer.combos['count'].sum(), 10)
         
     def test_face_counts_per_roll(self):
-        result = self.analyzer.face_counts_per_roll()
-        print(result)
-        self.assertIsInstance(result, pd.DataFrame)
+        self.analyzer.face_counts_per_roll()
+        self.assertIsInstance(self.analyzer.face_counts, pd.DataFrame)
+        self.assertEqual(len(self.analyzer.face_counts), 10)
+        self.assertTrue((self.analyzer.face_counts.sum(axis=1) == 2).all())
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=3)
